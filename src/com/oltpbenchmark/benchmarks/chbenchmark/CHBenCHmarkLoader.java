92e7ae42c9ba499d52eb5bfe00d7ce11eb748bfb
Dmytro Vorona <alendit@gmail.com>
2012-10-26 20:25:48 +0200

    Unused import removed

--- src/com/oltpbenchmark/benchmarks/chbenchmark/CHBenCHmarkLoader.java
+++ src/com/oltpbenchmark/benchmarks/chbenchmark/CHBenCHmarkLoader.java
@@ -5,7 +5,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOError;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
