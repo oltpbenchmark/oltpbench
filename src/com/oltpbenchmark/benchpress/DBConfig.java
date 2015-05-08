4ea70656ee690b4fec8060d8ce17a0453acb294a
Dana Van Aken <dvanaken@cs.cmu.edu>
2015-05-07 21:28:12 -0400

    partially integrated the benchpress SocketIO server

---
+++ src/com/oltpbenchmark/benchpress/DBConfig.java
@@ -0,0 +1,53 @@
+package com.oltpbenchmark.benchpress;
+public class DBConfig {
+
+    private String dbms;
+    private String benchmark;
+
+    public DBConfig() {
+    }
+
+    public DBConfig(String dbms, String benchmark) {
+        super();
+        this.dbms = dbms;
+        this.benchmark = benchmark;
+    }
+
+    public String getDbms() {
+        return dbms;
+    }
+    
+    public void setDbms(String dbms) {
+        this.dbms = dbms;
+    }
+
+    public String getBenchmark() {
+        return benchmark;
+    }
+    
+    public void setBenchmark(String benchmark) {
+        this.benchmark = benchmark;
+    }
+    
+    public boolean isValidDbms(String dbms) {
+        return benchmark.equals("mysql") || benchmark.equals("postgres");
+    }
+    
+    public boolean isValidBenchmark(String benchmark) {
+        return benchmark.equals("ycsb");
+    }
+    
+    public boolean isValid() {
+        return isValidBenchmark(benchmark) && isValidDbms(dbms);
+    }
+    
+    public void setDefaults() {
+        benchmark = "tpcc";
+        dbms = "mysql";
+    }
+
+    @Override
+    public String toString() {
+        return "\tDBMS: " + dbms + "\n\tBenchmark: " + benchmark + "\n\n";
+    }
+}
