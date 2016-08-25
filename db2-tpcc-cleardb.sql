4125f3f5a8ff8c542fe3a6bed6def124f11ffd12
Ritchie J Latimore <ritchie.latimore@bluemedora.com>
2016-08-25 15:38:44 +0000

    another formatting attempt

--- db2-tpcc-cleardb.sql
+++ db2-tpcc-cleardb.sql
@@ -1,43 +1,44 @@
 CREATE OR REPLACE PROCEDURE db2perf_quiet_drop( IN statement VARCHAR(1000) )
 LANGUAGE SQL
 BEGIN
-DECLARE SQLSTATE CHAR(5);
-DECLARE NotThere    CONDITION FOR SQLSTATE '42704';
-DECLARE NotThereSig CONDITION FOR SQLSTATE '42883';
+  DECLARE SQLSTATE CHAR(5);
+  DECLARE NotThere    CONDITION FOR SQLSTATE '42704';
+  DECLARE NotThereSig CONDITION FOR SQLSTATE '42883';
 
-DECLARE EXIT HANDLER FOR NotThere, NotThereSig
-SET SQLSTATE = '     ';
+  DECLARE EXIT HANDLER FOR NotThere, NotThereSig
+  SET SQLSTATE = '     ';
 
-SET statement = 'DROP ' || statement;
-EXECUTE IMMEDIATE statement;
+  SET statement = 'DROP ' || statement;
+  EXECUTE IMMEDIATE statement;
 END
 
 BEGIN ATOMIC
-CALL db2perf_quiet_drop('TABLE customer');
-END
+  CALL db2perf_quiet_drop('TABLE customer');
+END;
 
 
 BEGIN ATOMIC
-CALL db2perf_quiet_drop('TABLE district');
-END
+  CALL db2perf_quiet_drop('TABLE district');
+END;
 
 
 BEGIN ATOMIC
-CALL db2perf_quiet_drop('TABLE history');
-END
+  CALL db2perf_quiet_drop('TABLE history');
+END;
 
 BEGIN ATOMIC
-CALL db2perf_quiet_drop('TABLE oorder');
-END
+  CALL db2perf_quiet_drop('TABLE oorder');
+END;
 
 BEGIN ATOMIC
-CALL db2perf_quiet_drop('TABLE order_line');
-END
+  CALL db2perf_quiet_drop('TABLE order_line');
+END;
 
 BEGIN ATOMIC
-CALL db2perf_quiet_drop('TABLE stock');
-END
+  CALL db2perf_quiet_drop('TABLE stock');
+END;
 
 BEGIN ATOMIC
-CALL db2perf_quiet_drop('TABLE warehouse');
-END
+  CALL db2perf_quiet_drop('TABLE warehouse');
+END;
+
