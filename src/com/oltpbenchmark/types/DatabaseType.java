67ce7ccc5875aa78653ca15d9e6abe58be703a9b
Vicki Long <tovickilong@gmail.com>
2017-12-16 17:52:47 -0500

    Add files for Firebird TPCC benchmark.

--- src/com/oltpbenchmark/types/DatabaseType.java
+++ src/com/oltpbenchmark/types/DatabaseType.java
@@ -48,7 +48,8 @@ public enum DatabaseType {
     MONETDB("nl.cwi.monetdb.jdbc.MonetDriver", false, false),
     NUODB("com.nuodb.jdbc.Driver", true, false),
     TIMESTEN("com.timesten.jdbc.TimesTenDriver", true, false),
-    PELOTON("org.postgresql.Driver", false, false)
+    PELOTON("org.postgresql.Driver", false, false),
+    FIREBIRD("org.firebirdsql.jdbc.FBDriver", true, false)
     ;
     
     private DatabaseType(String driver, boolean escapeNames, boolean includeColNames) {
