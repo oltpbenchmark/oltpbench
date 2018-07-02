3443973638ac68fb834e905f58f662acea4cb024
Wan Shen Lim <wanshenl@andrew.cmu.edu>
2018-07-02 12:37:44 -0400

    Enhancement: [TPCH] Add primary and foreign key constraints for better cardinality estimates.

--- src/com/oltpbenchmark/benchmarks/tpch/ddls/tpch-memsql-ddl.sql
+++ src/com/oltpbenchmark/benchmarks/tpch/ddls/tpch-memsql-ddl.sql
@@ -10,11 +10,13 @@ DROP TABLE IF EXISTS lineitem;
 CREATE TABLE nation  ( n_nationkey  INTEGER NOT NULL,
                        n_name       CHAR(25) NOT NULL,
                        n_regionkey  INTEGER NOT NULL,
-                       n_comment    VARCHAR(152));
+                       n_comment    VARCHAR(152),
+                       KEY (`n_nationkey`) USING CLUSTERED COLUMNSTORE);
 
 CREATE TABLE region  ( r_regionkey  INTEGER NOT NULL,
                        r_name       CHAR(25) NOT NULL,
-                       r_comment    VARCHAR(152));
+                       r_comment    VARCHAR(152),
+                       KEY (`r_regionkey`) USING CLUSTERED COLUMNSTORE);
 
 CREATE TABLE part  ( p_partkey     INTEGER NOT NULL,
                      p_name        VARCHAR(55) NOT NULL,
@@ -24,7 +26,8 @@ CREATE TABLE part  ( p_partkey     INTEGER NOT NULL,
                      p_size        INTEGER NOT NULL,
                      p_container   CHAR(10) NOT NULL,
                      p_retailprice DECIMAL(15,2) NOT NULL,
-                     p_comment     VARCHAR(23) NOT NULL );
+                     p_comment     VARCHAR(23) NOT NULL,
+                     KEY (`p_partkey`) USING CLUSTERED COLUMNSTORE);
 
 CREATE TABLE supplier ( s_suppkey     INTEGER NOT NULL,
                         s_name        CHAR(25) NOT NULL,
@@ -32,13 +35,15 @@ CREATE TABLE supplier ( s_suppkey     INTEGER NOT NULL,
                         s_nationkey   INTEGER NOT NULL,
                         s_phone       CHAR(15) NOT NULL,
                         s_acctbal     DECIMAL(15,2) NOT NULL,
-                        s_comment     VARCHAR(101) NOT NULL);
+                        s_comment     VARCHAR(101) NOT NULL,
+                        KEY (`s_suppkey`) USING CLUSTERED COLUMNSTORE);
 
 CREATE TABLE partsupp ( ps_partkey     INTEGER NOT NULL,
                         ps_suppkey     INTEGER NOT NULL,
                         ps_availqty    INTEGER NOT NULL,
                         ps_supplycost  DECIMAL(15,2)  NOT NULL,
-                        ps_comment     VARCHAR(199) NOT NULL );
+                        ps_comment     VARCHAR(199) NOT NULL,
+                        KEY (`ps_partkey`, `ps_suppkey`) USING CLUSTERED COLUMNSTORE);
 
 CREATE TABLE customer ( c_custkey     INTEGER NOT NULL,
                         c_name        VARCHAR(25) NOT NULL,
@@ -47,7 +52,8 @@ CREATE TABLE customer ( c_custkey     INTEGER NOT NULL,
                         c_phone       CHAR(15) NOT NULL,
                         c_acctbal     DECIMAL(15,2)   NOT NULL,
                         c_mktsegment  CHAR(10) NOT NULL,
-                        c_comment     VARCHAR(117) NOT NULL);
+                        c_comment     VARCHAR(117) NOT NULL,
+                        KEY (`c_custkey`) USING CLUSTERED COLUMNSTORE);
 
 CREATE TABLE orders  ( o_orderkey       INTEGER NOT NULL,
                        o_custkey        INTEGER NOT NULL,
@@ -57,7 +63,8 @@ CREATE TABLE orders  ( o_orderkey       INTEGER NOT NULL,
                        o_orderpriority  CHAR(15) NOT NULL,  
                        o_clerk          CHAR(15) NOT NULL, 
                        o_shippriority   INTEGER NOT NULL,
-                       o_comment        VARCHAR(79) NOT NULL);
+                       o_comment        VARCHAR(79) NOT NULL,
+                       KEY (`o_orderkey`) USING CLUSTERED COLUMNSTORE);
 
 CREATE TABLE lineitem ( l_orderkey    INTEGER NOT NULL,
                         l_partkey     INTEGER NOT NULL,
@@ -74,31 +81,5 @@ CREATE TABLE lineitem ( l_orderkey    INTEGER NOT NULL,
                         l_receiptdate DATE NOT NULL,
                         l_shipinstruct CHAR(25) NOT NULL,
                         l_shipmode     CHAR(10) NOT NULL,
-                        l_comment      VARCHAR(44) NOT NULL);
-
-create index c_ck on customer (c_custkey asc) ;
-create index c_nk on customer (c_nationkey asc) ;
-create index p_pk on part (p_partkey asc) ;
-create index s_sk on supplier (s_suppkey asc) ;
-create index s_nk on supplier (s_nationkey asc) ;
-create index ps_pk on partsupp (ps_partkey asc) ;
-create index ps_sk on partsupp (ps_suppkey asc) ;
-create index ps_pk_sk on partsupp (ps_partkey asc, ps_suppkey asc) ;
-create index ps_sk_pk on partsupp (ps_suppkey asc, ps_partkey asc) ;
-create index o_ok on orders (o_orderkey asc) ;
-create index o_ck on orders (o_custkey asc) ;
-create index o_od on orders (o_orderdate asc) ;
-create index l_ok on lineitem (l_orderkey asc) ;
-create index l_pk on lineitem (l_partkey asc) ;
-create index l_sk on lineitem (l_suppkey asc) ;
-create index l_ln on lineitem (l_linenumber asc) ;
-create index l_sd on lineitem (l_shipdate asc) ;
-create index l_cd on lineitem (l_commitdate asc) ;
-create index l_rd on lineitem (l_receiptdate asc) ;
-create index l_ok_ln on lineitem (l_orderkey asc, l_linenumber asc) ;
-create index l_ln_ok on lineitem (l_linenumber asc, l_orderkey asc) ;
-create index l_pk_sk on lineitem (l_partkey asc, l_suppkey asc) ;
-create index l_sk_pk on lineitem (l_suppkey asc, l_partkey asc) ;
-create index n_nk on nation (n_nationkey asc) ;
-create index n_rk on nation (n_regionkey asc) ;
-create index r_rk on region (r_regionkey asc) ;
+                        l_comment      VARCHAR(44) NOT NULL,
+                        KEY (`l_orderkey`, `l_linenumber`) USING CLUSTERED COLUMNSTORE);
