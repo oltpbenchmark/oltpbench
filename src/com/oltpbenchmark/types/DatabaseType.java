5d9cb6da2018c6a2f6e610c8aebafbb06944478d
Tao Dai <tao.dai36@gmail.com>
2018-10-06 11:46:41 -0400

    add hana

--- src/com/oltpbenchmark/types/DatabaseType.java
+++ src/com/oltpbenchmark/types/DatabaseType.java
@@ -52,6 +52,7 @@ public enum DatabaseType {
     CASSANDRA("com.github.adejanovski.cassandra.jdbc.CassandraDriver", true, true, false),
     PELOTON("org.postgresql.Driver", false, false, true),
     MEMSQL("com.mysql.jdbc.Driver", true, false, false),
+    SAPHANA("com.sap.db.jdbc.Driver", true, false, true)
     ;
     
     private DatabaseType(String driver,
