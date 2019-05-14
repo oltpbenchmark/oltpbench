40028d3e2501eb1f046efa8a3dedc6f8bdc89993
Andy Pavlo <pavlo@cs.brown.edu>
2019-05-13 22:25:26 -0400

    Make sure that we pick the right hsqldb jar file to include. Ivy brings in v1.8 and that breaks our internal catalog loader

--- tests/com/oltpbenchmark/api/MockBenchmark.java
+++ tests/com/oltpbenchmark/api/MockBenchmark.java
@@ -49,7 +49,7 @@ public class MockBenchmark extends BenchmarkModule {
     public URL getDatabaseDDL(DatabaseType db_type) {
         // Get our sample DDL file
         URL testDDLURL = MockBenchmark.class.getResource("test-ddl.sql");
-        assert(testDDLURL != null);
+        assert(testDDLURL != null) : "Unable to get " + MockBenchmark.class.getSimpleName() + " DDL file";
         File testDDL = new File(testDDLURL.getPath());
         assert(testDDL.exists()) : testDDL.getAbsolutePath();
         return (testDDLURL);
