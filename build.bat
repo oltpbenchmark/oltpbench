42f331dc811f0115546138391cba6573c2901b98
Carl Camilleri <carl.camilleri@cs-technologies.net>
2019-07-14 16:33:28 +0200

    setup of ant tools for windows integrate with Vertica

---
+++ build.bat
@@ -0,0 +1,3 @@
+call .\tools\apache-ant-1.9.14\bin\ant bootstrap
+call .\tools\apache-ant-1.9.14\bin\ant resolve
+call .\tools\apache-ant-1.9.14\bin\ant build
--- src/com/oltpbenchmark/types/DatabaseType.java
+++ src/com/oltpbenchmark/types/DatabaseType.java
@@ -52,6 +52,7 @@ public enum DatabaseType {
     CASSANDRA("com.github.adejanovski.cassandra.jdbc.CassandraDriver", true, true, false),
     PELOTON("org.postgresql.Driver", false, false, true),
     MEMSQL("com.mysql.jdbc.Driver", true, false, false),
+	VERTICA("com.vertica.jdbc.Driver", true, false, false),
     ;
     
     private DatabaseType(String driver,
